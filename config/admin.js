1d210e14aae08f653df3165150afc22f3b59670c
Pratap Singh <ps6067966@gmail.com>
2022-09-02 15:59:59 +0530

    pss

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'dfd05f472789bf41d929186a5a4c8f21'),
+    secret: env("ADMIN_JWT_SECRET", "dfd05f472789bf41d929186a5a4c8f21"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
   },
 });
